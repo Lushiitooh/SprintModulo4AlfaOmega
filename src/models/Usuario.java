11d86cfef17642cd48c120cb4cd90b585b5ab075
Andres Sepulveda <bodyweightforce@gmail.com>
2023-07-11 19:55:34 -0400

    Primer commit

---
+++ src/models/Usuario.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private LocalDate fechaNacimiento;
+    private int run;
+}
