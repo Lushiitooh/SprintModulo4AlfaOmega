89574c7b46458b2c3ebb223a9d62f91706bc7abe
Andres Sepulveda <bodyweightforce@gmail.com>
2023-07-12 19:27:23 -0400

    Main

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        * - Método Main
+- Instancia clase Contenedor: crear 2 listas una de usuarios y una de capacitaciones
+- Menú principal con 9 opciones
+    - Almacenar Cliente
+    - Almacenar Profesional
+    - Almacenar Administrativo
+    - Listar Usuarios
+    - Listar Usuarios por tipo
+    - Eliminar Usuarios por run
+    - Almacenar Capacitación
+    - Listar Capacitaciones
+    - Salir
+
+Si ingresa opción incorrecta, debe avisar y volver a pedir una opción
+
+- Instanciar 5 objetos por cada tipo y probar el menú completo.
+        * */
 
         Administrativo u1 = new Administrativo();
 
