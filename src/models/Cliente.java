e163b45ffe0f8f81ce9c2a65fd1ec80a887f970e
Andres Sepulveda <bodyweightforce@gmail.com>
2023-07-11 20:56:37 -0400

    Second commit

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,4 +1,72 @@
 package models;
 
-public class Cliente {
+
+public class Cliente extends Usuario{
+    private String razonSocial;
+    private int RUN;
+    private long telefonoRepresentante;
+    private String direccionEmpresa;
+    private String comunaEmpresa;
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public int getRUN() {
+        return RUN;
+    }
+
+    public void setRUN(int RUN) {
+        this.RUN = RUN;
+    }
+
+    public long getTelefonoRepresentante() {
+        return telefonoRepresentante;
+    }
+
+    public void setTelefonoRepresentante(long telefonoRepresentante) {
+        this.telefonoRepresentante = telefonoRepresentante;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public String getComunaEmpresa() {
+        return comunaEmpresa;
+    }
+
+    public void setComunaEmpresa(String comunaEmpresa) {
+        this.comunaEmpresa = comunaEmpresa;
+    }
+
+    public String obtenerNombre() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Cliente.").append("\n")
+                .append("Razon social: ").append(this.razonSocial).append("\n")
+                .append("Rut: ").append(this.RUN).append("\n")
+                .append("Direccion: ").append(this.direccionEmpresa);
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "razonSocial='" + razonSocial + '\'' +
+                ", RUN=" + RUN +
+                ", telefonoRepresentante=" + telefonoRepresentante +
+                ", direccionEmpresa='" + direccionEmpresa + '\'' +
+                ", comunaEmpresa='" + comunaEmpresa + '\'' +
+                '}';
+    }
 }
--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -1,11 +1,92 @@
 package models;
 
-import java.time.LocalDate;
+import service.IUsuario;
 
-public class Usuario {
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Usuario implements IUsuario {
     private String nombre;
     private String apellido1;
     private String apellido2;
     private LocalDate fechaNacimiento;
     private int run;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido1, String apellido2, LocalDate fechaNacimiento, int run) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.fechaNacimiento = fechaNacimiento;
+        this.run = run;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = LocalDate.parse(fechaNacimiento);
+    }
+
+    public int getEdad() {
+        LocalDate fechaActual = LocalDate.now();
+        Period edad = Period.between(this.fechaNacimiento, fechaActual);
+        int aniosEdad = edad.getYears();
+
+        return aniosEdad;
+    }
+
+    public int getRun() {
+        return run;
+    }
+
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+    public void mostrarEdad() {
+        System.out.println("Mi edad es: " + getEdad());
+    }
+
+    @Override
+    public void analizarUsuario() {
+
+    }
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido1='" + apellido1 + '\'' +
+                ", apellido2='" + apellido2 + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", run=" + run +
+                '}';
+    }
 }
